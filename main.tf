7550d1f607cef86163b532a4ada3098eb62abea5
Tatiana <moldangrus1@gmail.com>
2024-06-24 11:04:36 -0500

    update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,16 @@
-module app {
+module "app" {
   source = "./module"
   namespace = "default"
   name = "wordpress"
   wait = false
   chart = "./application"
   values = [<<EOF
-
 replicaCount: 3
-
 image:
-  repository: nginx
+  repository: wordpress
   pullPolicy: IfNotPresent
   # Overrides the image tag whose default is the chart appVersion.
-  tag: ""
+  tag: "latest"
   EOF
   ]
 }
-
-
-
-
-
